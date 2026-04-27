b9e3b270fff0546bdc4c362e7b847acbc55d1da2
Yashi Singh <S24CSEU0258@bennett.edu.in>
2026-04-27 22:25:05 +0530

    fixed

--- planeto-app/planeto-backend/models/Note.js
+++ planeto-app/planeto-backend/models/Note.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const noteSchema = new mongoose.Schema({
   title: String,
@@ -8,4 +8,4 @@ const noteSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
 });
 
-module.exports = mongoose.model("Note", noteSchema);
+export default mongoose.model("Note", noteSchema);
