50a70efd66a4faa169b591c4929c4b033cdf1625
Yashi Singh <S24CSEU0258@bennett.edu.in>
2026-04-30 22:24:24 +0530

    nil 3

--- planeto-app/planeto-backend/server.js
+++ planeto-app/planeto-backend/server.js
@@ -29,12 +29,15 @@ app.get("/", (req, res) => {
 });
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
+    console.log("MONGO CONNECTED 🔥");  
+
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT);
+    app.listen(PORT, () => {
+      console.log("SERVER STARTED 🚀"); 
+    });
   })
-  .catch((err) => console.log(err));
+  .catch((err) => {
+    console.log("MONGO ERROR:", err);
+  });
